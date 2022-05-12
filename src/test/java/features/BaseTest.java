ed74adae9057f961bc2b20c97b0b562c43f7b0c7
phuong.nguyenph <phuong30887@yahoo.com>
2022-05-12 14:46:10 +0700

    API flow: config - completed

--- src/test/java/features/BaseTest.java
+++ src/test/java/features/BaseTest.java
@@ -13,11 +13,15 @@ import static utilites.LoadConfig.CONFIG;
 public class BaseTest {
     @BeforeAll
     static void beforeAll() {
+        // phần này để pass qua https certificate khi link là http
         RestAssured.useRelaxedHTTPSValidation();
         RestAssured.defaultParser = Parser.JSON;
+        // filter cho restassured
         RestAssured.replaceFiltersWith(new AllureRestAssured());
+        // config để default content sẽ là UTF-8 cho trường hợp dùng cho tiếng việt
         RestAssured.config=RestAssured.config()
                 .encoderConfig(encoderConfig().defaultContentCharset("UTF-8"));
+        // config cho default URI phù hợp cho tất cả môi trường : QA, ACC, Pro...
         RestAssured.baseURI = CONFIG.getProperty(BASE_URL);
 
     }
